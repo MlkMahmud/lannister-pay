70863fba5e3ec763a439e9ab8b9d8e7a7257e5e4
Malik Mahmud <almalikmahmud@gmail.com>
2022-03-20 17:43:44 +0100

    add upload-sourcemaps script

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import os from 'os';
 import app from './app';
 import redis from './lib/redis';
 import logger from './lib/logger';
@@ -7,8 +8,13 @@ const PORT = process.env.PORT || 3000;
 (async function start() {
   try {
     await redis.connect();
-    app.listen(PORT, () => {
-      logger.info(`> Running on port: ${PORT}`);
+    const server = app.listen(PORT, () => {
+      logger.info(
+        '> Lannister Pay is listening at http://%s:%s - env %s',
+        os.hostname(),
+        server.address().port,
+        process.env.NODE_ENV,
+      );
     });
   } catch (e) {
     logger.error(e);
