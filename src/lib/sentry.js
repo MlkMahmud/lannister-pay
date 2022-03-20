f0323c8c56affd8a3a048b5c9e5f39fe7a977b73
Malik Mahmud <almalikmahmud@gmail.com>
2022-03-20 17:27:37 +0100

    chore: add release property to sentry config

--- src/lib/sentry.js
+++ src/lib/sentry.js
@@ -1,8 +1,13 @@
 import * as Sentry from '@sentry/node';
 
+const version = process.env.SENTRY_RELEASE
+  ? process.env.SENTRY_RELEASE.trim()
+  : 'current';
+
 Sentry.init({
   dsn: process.env.SENTRY_DSN,
   enabled: process.env.NODE_ENV === 'production',
+  release: `lannister-pay@${version}`,
 });
 
 export default Sentry;
